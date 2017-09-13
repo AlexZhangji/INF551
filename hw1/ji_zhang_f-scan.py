ed981986a4106c3cbe1592e44c63eff6f185f8aa
zhang ji <zhan586@usc.edu>
2017-09-13 16:44:32 -0700

    for scan, 199 and 0 now not shown in the path print. need better way to handle this, current solution is ugly as fuck

--- hw1/ji_zhang_f-scan.py
+++ hw1/ji_zhang_f-scan.py
@@ -8,14 +8,17 @@ def parse_data(fname):
         return int(text_list[0]), text_list[1]
 
 
-def f_scan(head_pos_, pos_queue_):
+def f_scan(head_pos_, pos_queue_, added_list=[]):
     path = []
 
     while len(pos_queue_) > 10:
-        first_list = sorted(pos_queue_[:11])
+        first_list = sorted(pos_queue_[:10])
         # copy list of rest posistions, in case 199 and 0 added to mess up the index
-        rest_list = pos_queue_[11:]
-        path += ji_zhang_scan.scan(head_pos_, first_list)
+        rest_list = pos_queue_[10:]
+        cur_path, cur_added_list = ji_zhang_scan.scan(head_pos_, first_list)
+        added_list += [cur_index + (len(path) / 10) * 10 for cur_index in cur_added_list]  # yea... hacky and ugly
+
+        path += cur_path
 
         # update head position and position list
         head_pos_ = path[-1]
@@ -23,15 +26,24 @@ def f_scan(head_pos_, pos_queue_):
 
     if len(pos_queue_) <= 10:
         pos_queue_ = sorted(pos_queue_)
-        path += ji_zhang_scan.scan(head_pos_, pos_queue_)
+        cur_path, cur_added_list = ji_zhang_scan.scan(head_pos_, pos_queue_)
+        added_list += [cur_index + (len(path) / 10)*10 for cur_index in cur_added_list]  # yea... super hacky and ugly
 
-    return path
+        path += cur_path
+
+    return path, added_list
 
 
-def print_result(res_path, head_pos_):
+def print_result(res_path, head_pos_, added_list):
     tot_cost = abs(head_pos_ - res_path[0])
     for i in range(len(res_path) - 1):
         tot_cost += abs(res_path[i] - res_path[i + 1])
+
+    # clean added 199 and 0
+    for index in enumerate(res_path):
+        index = int(index[0])
+        if index in added_list:
+            del res_path[index]
 
     res_path_str = ','.join(str(x) for x in res_path)
     print res_path_str, '\n', tot_cost, '\n', '{},{}'.format(res_path[-1], tot_cost)
@@ -44,5 +56,7 @@ head_pos, raw_pos_queue = parse_data(fname)
 print 'head:{}'.format(head_pos)
 # not same with scan or sstf, not sorted list
 pos_queue = ([int(num) for num in raw_pos_queue.split(',')])
-result_path = f_scan(head_pos, pos_queue)
-print_result(result_path, head_pos)
+result_path, added_list = f_scan(head_pos, pos_queue)
+# print result_path
+print 'added_list: {}'.format(added_list)
+print_result(result_path, head_pos, added_list)
--- hw1/ji_zhang_scan.py
+++ hw1/ji_zhang_scan.py
@@ -26,9 +26,12 @@ def scan(head_pos_, pos_queue_):
     left = True
     cur_head_index = 0
 
+    zero_added = False
+    end_added = False
+
     if len(pos_queue_) == 0:
         # print path
-        return path
+        return path, []
     # if two has same distance, head will move to inner most stack
     next_head = get_closest_num(head_pos_, pos_queue_)
 
@@ -48,11 +51,15 @@ def scan(head_pos_, pos_queue_):
     if 199 not in pos_queue_ and head_pos_ < next_head and head_pos_ != 199:
         pos_queue_.append(199)
         left = False
+        end_added = True
+
+
 
     elif 0 not in pos_queue_ and head_pos_ > next_head and head_pos_ != 0:
         pos_queue_.insert(0, 0)
         left = True
-        cur_head_index += 1
+        cur_head_index += 1  # this is needed as it inserted in the beginning of the list
+        zero_added = True
 
     # break the list to two parts, first iterate the one
     if left:
@@ -60,8 +67,15 @@ def scan(head_pos_, pos_queue_):
     else:
         path += pos_queue_[cur_head_index:] + list(reversed(pos_queue_[:cur_head_index]))
 
+    print path
+    added_index_list = []
+    if zero_added:
+        added_index_list.append(path.index(0))
+    if end_added:
+        added_index_list.append(path.index(199))
+
     # print 'path:{}'.format(path)
-    return path
+    return path, added_index_list
 
 
 # fname = 'test1.txt'
@@ -73,13 +87,20 @@ if sys.argv[1:]:
     # print head_pos, pos_queue
 
     pos_queue = sorted([int(num) for num in raw_pos_queue.split(',')])
-    # if pos_queue does not has 199, add to it
+    origin_queue = pos_queue[:]
 
-    result_path = scan(head_pos, pos_queue)
+    result_path, add_index_list = scan(head_pos, pos_queue)
 
     tot_cost = abs(head_pos - result_path[0])
     for i in range(len(result_path) - 1):
         tot_cost += abs(result_path[i] - result_path[i + 1])
+
+    # print 'added list:{}'.format(add_index_list)
+    # clean added 199 and 0
+    for index in enumerate(result_path):
+        index = int(index[0])
+        if index in add_index_list:
+            del result_path[index]
 
     res_path_str = ','.join(str(x) for x in result_path)
     print res_path_str, '\n', tot_cost, '\n', '{},{}'.format(result_path[-1], tot_cost)
