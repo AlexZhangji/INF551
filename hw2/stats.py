41238b88a768e2a57781b11fccf8f37405cd05dc
zhang ji <zhan586@usc.edu>
2017-09-27 10:55:35 -0700

    1b kinda finished, need clarify on how search keywords work. result json added

--- hw2/stats.py
+++ hw2/stats.py
@@ -50,17 +50,30 @@ def search(terms, questions):
     res_list = []
     # assume that the key words are separated by the comma,
     # convert from 'barca, andrews iniesta' to [barca, andrews, iniesta]
-    term_set = set(' '.join(terms.split(',')).split(' '))
+    term_set = set(' '.join(terms.lower().split(',')).split(' '))
+    print 'term set :{}'.format(term_set)
     for question in questions:
-        cur_q = re.sub('[^0-9a-zA-Z]+', '', question['question'])  # clean all non-alphanumeric strings
+        cur_q = re.sub('[^0-9a-zA-Z ]+', '', question['question'])  # clean all non-alphanumeric strings
         cur_q_set = set(cur_q.lower().split(' '))  # set of lowercase words in question
+
+        # print cur_q
+        # print cur_q_set
 
         if not term_set.isdisjoint(cur_q_set):  # if two set share words
             temp_dict = question
-            temp_dict['question'] = question['question']
-    
+            temp_dict['answer'] = temp_dict['answers'][0]['text']
+            # remove the answers entry from dict
+            del temp_dict['answers']
+
+            res_list.append(temp_dict)
+
+    with open('1b_search_res.json', 'w') as f:
+        r = json.dumps(res_list)
+        f.write(r)
+
 
 fname = 'qa.json'
 raw_json = get_json(fname)
 q_json = get_list_questions(raw_json)
 get_question_stats(q_json)
+search('determined', q_json)
