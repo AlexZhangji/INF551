1c94cb7c5ef5424af69e21030c93c4517aec3665
zhang ji <zhan586@usc.edu>
2017-10-18 11:36:32 -0700

    hw3 almost done

---
+++ hw3/test_connection.py
@@ -0,0 +1,28 @@
+import sys
+
+import mysql.connector
+
+
+def search_query(term, cnx):
+    cursor = cnx.cursor()
+
+    cursor.execute("SELECT " \
+                   "COUNT( film_category.category_id ) AS category_count " \
+                   "FROM " \
+                   "film_category " \
+                   "JOIN category ON category.category_id = film_category.category_id " \
+                   "WHERE LOWER(category.NAME) LIKE LOWER(%s);", (term,))
+
+    for name in cursor:
+        print name
+
+    cursor.close()
+    cnx.close()
+
+
+if sys.argv[1:]:
+    term = sys.argv[1:][0]
+    cnx = mysql.connector.connect(user='root', password='root',
+                                  host='127.0.0.1',
+                                  database='sakila')
+    search_query(term, cnx)
